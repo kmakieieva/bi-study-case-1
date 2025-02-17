3e6536bce071f5b76b60680aa7d67a0624f7eea7
Kateryna <makieieva.k@gmail.com>
2025-02-17 17:20:49 -0500

    Added predictive analysis of sentiment scores

--- bi_study_case_1.py
+++ bi_study_case_1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+from prophet import Prophet
 
 nltk.download('vader_lexicon')
 
@@ -30,6 +31,7 @@ def transform(place):
 reviews = list(map(transform, places))
 reviewsList = []
 ratingsList = []
+datesList = []
 
 for review_list in reviews:
     for review in review_list:
@@ -38,10 +40,12 @@ for review_list in reviews:
         text = text.replace('\n', ' ')
         reviewsList.append(text)
         ratingsList.append(review['rating'])
+        datesList.append(review['publishTime'])
 
 data = {
     'review': reviewsList,
-    'rating': ratingsList
+    'rating': ratingsList,
+    'date': datesList
 }
 df = pd.DataFrame(data)
 
@@ -65,4 +69,36 @@ plt.title('Comparison: Rating vs. VADER Sentiment Score')
 plt.xlabel('Rating')
 plt.ylabel('Sentiment Score')
 plt.grid(True)
+plt.show()
+
+
+# Predictive analysis of sentiment scores
+def categorize_sentiment(score):
+    if score > 0.05:
+        return 'positive'
+    elif score < -0.05:
+        return 'negative'
+    else:
+        return 'neutral'
+
+
+df['sentiment'] = df['sentiment_score'].apply(categorize_sentiment)
+df['date'] = pd.to_datetime(df['date'])
+daily_sentiment = df.groupby(df['date'].dt.date)['sentiment_score'].mean().reset_index()
+daily_sentiment['cap'] = 1
+daily_sentiment['floor'] = -1
+
+prophet_df = daily_sentiment.rename(columns={'date': 'ds', 'sentiment_score': 'y'})
+model = Prophet(growth='logistic', weekly_seasonality=False, daily_seasonality=True)
+model.fit(prophet_df)
+future = model.make_future_dataframe(periods=120)
+future['cap'] = 1
+future['floor'] = -1
+
+forecast = model.predict(future)
+fig1 = model.plot(forecast)
+ax = fig1.gca()
+ax.set_xlabel('Date')
+ax.set_ylabel('Sentiment Score')
+ax.set_title('Future Sentiment Score Prediction')
 plt.show()
